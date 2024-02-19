5a63f3548ef19f32c9c1a0c21ed8d773ea1ebad5
Jeremy Quievreux <jquievreux.devpro@gmail.com>
2024-02-19 14:03:50 +0100

    Update cards.js

--- data/cards.js
+++ data/cards.js
@@ -12,46 +12,54 @@ export const cards = [
   {
     id: "001",
     name: "Sierra & Oddball",
-    pic_url: image001,
+    pic_url: "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_02/JPG/7e5b29374278b12806fac1b987fc64b8.jpg",
   },
   {
     id: "002",
     name: "Treyst & Rossum",
-    pic_url: image002,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_02/JPG/7e5b29374278b12806fac1b987fc64b8.jpg",
   },
   {
     id: "003",
     name: "Elementaire de Kélon C",
-    pic_url: image003,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_04/JPG/d1ab7926496554e968bf3609d0a2cdb9.jpg",
   },
   {
     id: "004",
     name: "Elementaire de Kélon R",
-    pic_url: image004,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_04/JPG/fc40643ff847abc698aa446f97ed5625.jpg",
   },
   {
     id: "005",
     name: "Elementaire de Kélon F",
-    pic_url: image005,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_04/JPG/9295458f656a6e7ca9bd94a66f70c182.jpg",
   },
   {
     id: "006",
     name: "Mécanicienne de la Fonderie C",
-    pic_url: image006,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_07/JPG/ba7cab9db2e76ff2740b522a56b43604.jpg",
   },
   {
     id: "007",
     name: "Mécanicienne de la Fonderie R",
-    pic_url: image007,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_07/JPG/2618480af017118dd8e59c033cfefa93.jpg",
   },
   {
     id: "008",
     name: "Mécanicienne de la Fonderie F",
-    pic_url: image008,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_07/JPG/9b5204c6ccd1f147022dfc26dc12fb6a.jpg",
   },
   {
     id: "009",
     name: "Récupérateur Axiom",
-    pic_url: image009,
+    pic_url:
+      "https://altered-prod-eu.s3.amazonaws.com/Art/CORE/CARDS/ALT_CORE_B_AX_08/JPG/712714b01813f326b256aa70530bccfb.jpg",
   },
 ];
