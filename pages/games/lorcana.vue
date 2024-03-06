ffaf88522d1b11910b2cf646ea59e316cb4130a3
Quievreux <jquievreux.devpro@gmail.com>
2024-03-06 19:44:33 +0100

    add lorcana's card

--- pages/games/lorcana.vue
+++ pages/games/lorcana.vue
@@ -1,13 +1,72 @@
 <template>
-    <div class="lorcana-page-container">
-        <h1>Lorcana Page</h1>
+    <div class="altered-page-container">
+        <div class="main-container">
+            <div class="cards-container">
+                <div v-for="(card, index) in cards" :key="index" class="card-container">
+                    <img :src="card.pic_url" alt="">
+                    <button>Add</button>
+                </div>
+            </div>
+            <div class="deck-container">
+
+            </div>
+        </div>
     </div>
 </template>
-<style lang="scss">
-.lorcana-page-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: calc(100vh - 50px);
+<script>
+import { cards } from '../data/lorcanaCards';
+
+export default {
+    data() {
+        return {
+            cards: [...cards]
+        }
+    }
 }
-</style>
+</script>
+<style lang="scss">
+@import "../../style/color.scss";
+
+.altered-page-container {
+    .main-container {
+        display: flex;
+        justify-content: space-around;
+        padding-top: 20px;
+
+        .cards-container {
+            border: 2px solid $tertiary;
+            width: 70%;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+
+            .card-container {
+                display: flex;
+                flex-direction: column;
+                max-width: 250px;
+                justify-content: center;
+                align-items: center;
+                margin: 10px;
+
+                img {
+                    width: 100%;
+                    border-radius: 15px;
+                    margin-bottom: 10px;
+                }
+
+                button {
+                    margin: 10px;
+                }
+
+                &:hover {
+                    scale: 125%;
+                }
+            }
+        }
+
+        .deck-container {
+            border: 2px solid $tertiary;
+            width: 25%;
+        }
+    }
+}</style>
